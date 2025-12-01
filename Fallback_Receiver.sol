c08b502ddfe8f7129b3502336936f2d022777eec
BALAJI A <152677288+Balajih4kr@users.noreply.github.com>
2025-12-01 20:28:05 +0530

    Implement Userstorage contract with balance management

---
+++ Fallback_Receiver.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Userstorage{
+
+    event Received(address sender, uint amount);
+    event FallbackCalled(address sender, uint amount, bytes data); 
+    event Withdrawn(address receiver, uint amount);
+
+    mapping (address => uint) public balance;
+
+    receive() external payable {
+        balance[msg.sender] += msg.value;
+        emit Received(msg.sender, msg.value);
+    }
+
+    fallback() external payable {
+        balance[msg.sender] += msg.value;
+        emit FallbackCalled(msg.sender, msg.value, msg.data);
+    }
+
+    function Withdraw(uint _amount) external  {
+        require(balance[msg.sender] >= _amount, "Not enough Balance");
+
+        balance[msg.sender] -= _amount;
+        payable(msg.sender).transfer(_amount);
+
+        emit Withdrawn(msg.sender, _amount);
+    }
+
+    function myBalance() external view returns(uint) 
+    { 
+        return balance[msg.sender]; 
+    }
+}
